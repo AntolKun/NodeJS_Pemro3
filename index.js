d8cfe057edde0a7c4db957a0138e5c3cef00c5bc
AntolKun <aloysiusluilui@gmail.com>
2024-06-29 19:19:46 +0700

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes/routes");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const mongoose = require("mongoose");
 
